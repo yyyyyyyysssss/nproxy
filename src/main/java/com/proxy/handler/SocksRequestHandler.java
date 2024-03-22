19be8606644c68a18ea314da49595f66aca884b1
ys <Shuai.Yan@keyrus.com>
2024-03-22 17:44:35 +0800

    优化日志打印

--- src/main/java/com/proxy/handler/SocksRequestHandler.java
+++ src/main/java/com/proxy/handler/SocksRequestHandler.java
@@ -1,7 +1,6 @@
 package com.proxy.handler;
 
 import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.handler.codec.socks.*;
